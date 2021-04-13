f1014cdb1e22e926d7d81b55b5e759facd71ef30
mazurenkooleksandr <kakchtoinfo@ukr.net>
2021-04-13 07:34:35 +0300

    commit

--- js/main.js
+++ js/main.js
@@ -1,18 +1,29 @@
 cc.game.onStart = function(){
-  let iconsPlist = ["img/icons.plist"];
+    
+    let iconsPlist = ["img/icons.plist"];
         cc.LoaderScene.preload(iconsPlist, function () {
             let simpleScene = cc.Scene.extend({
                 onEnter() {
+                    this._super();  
                     let spinButton = ccui.Button.create("img/button_spin.png");
-                    let background = cc.Sprite.create("img/bg.png");
-                    let redLine = document.querySelector(".slots__red_line");
+                    let background = cc.Sprite.create("img/bg.jpg");
                     let result = cc.LabelTTF.create("Result: Welcome", "Nunito", 50);
                     let size = cc.director.getWinSizeInPixels();
                     let start = false;  
                     let slots = [];
                     result.setFontFillColor("black"); 
                     cc.spriteFrameCache.addSpriteFrames("img/icons.plist");
-                    this._super();  
+                    
+                    let sprite = new cc.Sprite.create("img/ship.png");
+                    sprite.setAnchorPoint(cc.p(0.5, 0.5));
+                    sprite.setScale(0.3);
+                    sprite.setPosition(cc.p(size.width/1.28, size.height/9));
+                    this.addChild(sprite, 1);
+
+                    
+                    
+                    cc.log(sprite);
+                    
                 
                     function getRandomInt(int) {
                         return Math.random() * int;
@@ -27,9 +38,8 @@ cc.game.onStart = function(){
                         if(start) {
                             return;
                         }
-
+                        
                         start = true;
-                        redLine.style.display = "none";
                         spinButton.loadTextures("img/button_spin_blocked.png");
                         result.setString("Result: Spinning");
                       
@@ -45,7 +55,7 @@ cc.game.onStart = function(){
                             checkWinnner();
                             start = false;
                             spinButton.loadTextures("img/button_spin.png");
-                        }, 10000);
+                        }, 3000);
                     }; 
                   
                     let self = this;
@@ -55,13 +65,14 @@ cc.game.onStart = function(){
                         self.addChild(texture, z_index);
                     };
 
-                        textureSetting(result, 3.5, 8, 1, 1);
-                        textureSetting(spinButton, 1.27, 8, 1.5, 1);
+                        textureSetting(result, 4.5, 9, 1, 1);
+                        textureSetting(spinButton, 3.07, 3.5, 1.5, 1);
                         textureSetting(background, 2.05, 1.99, 1.4, 0);
+                        
                    
                     let slotsPositioning = function(initialIndex, maxIndex, y_Index) {
-                        let Y_pos = [1.32, 1.8, 2.8],
-                            X_pos = [4.4, 2, 1.3],
+                        let Y_pos = [1.12, 1.45, 2.1],
+                            X_pos = [6.2, 3.1, 2.08],
                             index = 0;
                         
                         for (let i = initialIndex; i < maxIndex; i++) {
@@ -75,12 +86,54 @@ cc.game.onStart = function(){
                         slotsPositioning(6,9,2); 
                  
                     function checkWinnner() {
-                        if (slots[3].id == slots[4].id || slots[3].id == slots[5].id || slots[4].id == slots[5].id) {
-                            result.setString("Result: Win");
-                            redLine.style.display = "block";
-                        } else {
-                            result.setString("Result: Not Win");
+
+                        if(x == 1421.875, y == 93.33333333333333){
+                            if(slots[3].id == slots[4].id || slots[3].id == slots[5].id || slots[4].id == slots[5].id){
+                                result.setString("Result: Go to Pluto");
+                                av = sprite.setPosition(cc.p(size.width/2.28, size.height/9));
+                               
+                            }else {
+                                result.setString("Result: Not Win"); 
                         }
+
+                        
+                    
+                        
+                        // if(b){
+                        //     if(slots[3].id == slots[4].id || slots[3].id == slots[5].id || slots[4].id == slots[5].id){
+                        //         result.setString("Result: Go to sssluto");
+                        //     //    c = sprite.setPosition(cc.p(size.width/3.28, size.height/9)); 
+                        //     //    b= null;
+                        //     }else {
+                        //         result.setString("Result: Not Win"); 
+                        // }
+
+                        // if(c){
+                        //     if(slots[3].id == slots[4].id || slots[3].id == slots[5].id || slots[4].id == slots[5].id){
+                        //         result.setString("Result: Go to 888luto");
+                        //         sprite.setPosition(cc.p(size.width/4.28, size.height/9)); 
+                        //     }else {
+                        //         result.setString("Result: Not Win"); 
+                        //     }
+                        // }
+                    }
+
+                    
+
+                        
+                            
+                            // if ((size.width/1.28, size.height/9) && slots[3].id == slots[4].id || slots[3].id == slots[5].id || slots[4].id == slots[5].id){
+                            //     result.setString("Result: Go to Pluto");
+                            //     textureSetting(ship, 1.44, 9, 0.3, 1); 
+                            // }else if ((size.width/1.44, size.height/9) && slots[3].id == slots[4].id || slots[3].id == slots[5].id || slots[4].id == slots[5].id){
+                            //     result.setString("Result: Go to ddd");
+                            //     textureSetting(ship, 3.44, 9, 0.3, 1); 
+                            // }else {
+                            //     result.setString("Result: Not Win");    
+                            // }      
+                            
+                           
+                        
                     };
 
                     function touchEvent(sender,  type) {
